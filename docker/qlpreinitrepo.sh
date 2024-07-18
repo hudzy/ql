5748fb486638db4ecfb96d7562eddab6f7bc805e
hudson@wsl-5900x <hudson@example.com>
2024-07-18 23:29:03 +0800

    update on 2024-07-18T15:29:03Z

--- docker/qlpreinitrepo.sh
+++ docker/qlpreinitrepo.sh
@@ -10,6 +10,7 @@ patch_version
 reload_update
 reload_pm2
 
+sed -i 's/^RepoFileExtensions=.*/RepoFileExtensions="js mjs py pyc sh"/' /ql/data/config/config.sh
 /ql/shell/update.sh repo https://github.com/hudzy/ql.git                   "task_"
 /ql/shell/update.sh repo https://github.com/6dylan6/jdpro.git              "jd_|jx_|jddj_" "backUp" "^jd[^_]|USER|JD|function|sendNotify|utils"
 /ql/shell/update.sh repo https://github.com/RayWangQvQ/BiliBiliToolPro.git "bili_task_"
