7a53f17c811165f5fb2d228149262d85becf46e7
hudson@wsl-5900x <hudson@example.com>
2024-07-18 20:08:52 +0800

    update on 2024-07-18T12:08:52Z

--- docker/qlpreinitrepo.sh
+++ docker/qlpreinitrepo.sh
@@ -6,5 +6,6 @@ dir_shell=/ql/shell
 
 reload_pm2
 
+ql repo https://github.com/hudzy/ql.git                   "task_"
 ql repo https://github.com/6dylan6/jdpro.git              "jd_|jx_|jddj_" "backUp" "^jd[^_]|USER|JD|function|sendNotify|utils"
 ql repo https://github.com/RayWangQvQ/BiliBiliToolPro.git "bili_task_"
